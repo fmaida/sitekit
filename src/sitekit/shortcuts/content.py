32eb4797f534e180bf513aa42014dc4ce14473b1
Francesco Maida <francesco.maida@gmail.com>
2025-10-14 15:24:31 +0200

    Aggiunto PageBundle

--- src/sitekit/shortcuts/content.py
+++ src/sitekit/shortcuts/content.py
@@ -5,5 +5,11 @@ from sitekit.settings import settings
 
 
 def load(*path: str) -> dict | None:
+    """
+    Shortcut di sitekit.cache.load per caricare
+    un file di configurazione dalla cartella content
+    predefinita
+    """
+
     percorso_completo = settings.CONTENT_DIR.joinpath(*path)
     return cache.load(percorso_completo)
--- src/sitekit/shortcuts/i18n.py
+++ src/sitekit/shortcuts/i18n.py
@@ -5,5 +5,11 @@ from sitekit.settings import settings
 
 
 def load(*path: str) -> dict | None:
+    """
+    Shortcut di sitekit.cache.load per caricare
+    un file di configurazione dalla cartella i18n
+    predefinita
+    """
+
     percorso_completo = settings.I18N_DIR.joinpath(*path)
     return cache.load(percorso_completo)
