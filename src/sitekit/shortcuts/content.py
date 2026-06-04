3e8b886ff2d88d53763df0bf42351e9a5763f8c3
Francesco Maida <francesco.maida@gmail.com>
2026-06-04 17:31:24 +0200

    Aggiunta la nuova classe Router per gestire i ridirezionamenti da URL verso percorsi sul disco

--- src/sitekit/shortcuts/content.py
+++ src/sitekit/shortcuts/content.py
@@ -4,12 +4,16 @@ from sitekit import cache
 from sitekit.settings import settings
 
 
-def load(*path: str) -> dict | None:
+def load(*path: str | Path) -> dict | None:
     """
     Shortcut di sitekit.cache.load per caricare
     un file di configurazione dalla cartella content
-    predefinita
+    predefinita.
+
+    Accetta stringhe e istanze di Path. Se viene passata una Path
+    assoluta, viene usata direttamente senza prefissare CONTENT_DIR.
     """
 
     percorso_completo = settings.CONTENT_DIR.joinpath(*path)
+
     return cache.load(percorso_completo)
